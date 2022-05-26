6eefb54d2da29e7f43e3db7cdf37cf5496f5ca36
ElizavetaShavlova <eliza260926@gmail.com>
2022-05-26 23:08:41 +0300

    configuration: add packages, wdio.conf, .gitignore, .gitattributes

---
+++ test/specs/smoke.js
@@ -0,0 +1,9 @@
+describe('Website', () => {
+    it('should be alive', () => {
+        browser.url(`/`)
+        const img = $('img[src="https://static.litecart.net/storage/images/logotype_light.svg"]')
+        if (!img.isExisting) {
+            throw new Error('Website should be opened with displayed logo');
+        }
+   });     
+});
